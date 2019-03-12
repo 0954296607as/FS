f4f640d39645d9be9bd329b9c770d0203048f01d
0954296607as <0954296607as@gmail.com>
2019-03-12 11:45:47 +0200

    Add html and css

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express= require ('express');
 const server = express ();
 const parser =require('body-parser');
+
 server.use(parser());
 
 
 
 
 server.use(express.static(__dirname+"/public"));
-server.get("/", (req,res)=>sendFile(index.html));
+
+server.get("/", (req,res)=>res.send(index.html))
+
+
 server.post('/registration/',(req,res)=>{
     console.log(req.body);
     res.send("all right");
